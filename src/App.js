2f9402f9348a7b441d706ababd8f6eb576cf26f6
Mahmoud Ashraf <mashraffarouk2702@gmail.com>
2023-07-26 02:32:56 +0300

    minor changes
    add check to items and fix responsiveness

--- src/App.js
+++ src/App.js
@@ -12,15 +12,20 @@ const App = () => {
   }
 
   const handleDelete = (id) => {
-    setItems((items) => items.filter((item) => item.id !== id))
+    setItems((items => items.filter((item) => item.id !== id)))
   }
+
+  const handleToggle = (id) => {
+    setItems((items) => items.map(item => item.id === id ? { ...item, packed: !item.packed } : item))
+  }
+
 
   return (
     <div className="app">
       <Logo />
       <Form OnAddItems={handleNewItem} />
-      <PackingList items={items} onDelete={handleDelete} />
-      <Stats />
+      <PackingList setItems={setItems} items={items} OnDeleteItems={handleDelete} OnToggleItem={handleToggle} />
+      <Stats items={items} />
     </div>
   );
 };
@@ -54,7 +59,7 @@ const Form = ({ OnAddItems }) => {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>what do you need for your trip?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+      <select onChange={(e) => setQuantity(+e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((number) => (
           <option key={number} value={number}>
             {number}
@@ -74,38 +79,43 @@ const Form = ({ OnAddItems }) => {
   );
 };
 
-const PackingList = ({ items, onDelete }) => {
+const PackingList = ({ items, OnDeleteItems, OnToggleItem }) => {
 
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item onDelete={onDelete} item={item} key={item.id} />
+          <Item OnDeleteItems={OnDeleteItems} item={item} key={item.id} OnToggleItem={OnToggleItem} />
         ))}
       </ul>
     </div>
   );
 };
 
-const Item = ({ item, onDelete }) => {
+const Item = ({ item, OnDeleteItems, OnToggleItem }) => {
 
   return (
     <li>
+      <input type="checkbox" value={item.packed} onChange={() => { OnToggleItem(item.id) }} />
       <span
         style={item.packed === true ? { textDecoration: "line-through" } : {}}
       >
         {item.quantity}x {item.description}
       </span>
-      <button onClick={() => onDelete(item.id)} className="deleteItem">&times;</button>
+      <button onClick={() => OnDeleteItems(item.id)} className="deleteItem">&times;</button>
     </li>
   );
 };
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (!items.length) return <em> <footer className="stats">Start adding some items to your packing list</footer></em>
+  const numberOfItems = items.length;
+  const numberOfPacked = items.filter(item => item.packed).length
+  const percentage = Math.round((numberOfPacked / numberOfItems) * 100)
   return (
-    <footer className="stats">
-      You have X items on your list, and you already packed Y (Z%)
-    </footer>
+    <em> <footer className="stats">
+      {percentage === 100 ? "You got everything! Ready to go" : `You have ${numberOfItems} items on your list, and you already packed ${numberOfPacked} (${percentage || 0}%)`}
+    </footer></em>
   );
 };
 
